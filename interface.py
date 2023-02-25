2127009cf955ddb7587536a167b626683ca62b64
Reese Butler <reesebutler@live.com>
2023-02-25 15:34:31 -0500

    implemented mouse support!
    * mouse movement, scrolling, and clicking are all supported
    * mouse input is only grabbed when the "lock to window" shortcut toggle is on
    * scroll direction can be inverted via optional .env variable

--- interface.py
+++ interface.py
@@ -2,7 +2,8 @@ import sys, pygame
 from key_mapping import keymap
 from usbtousb import UsbToUsb
 from usbtousb import KeyAction
-from pprint import pprint
+from usbtousb import MouseButton
+from usbtousb import MouseScrollDirection
 from string import ascii_lowercase
 from threading import Event
 from threading import Thread
@@ -19,26 +20,28 @@ black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
 
-blocked = [
-    pygame.WINDOWMOVED,
-    pygame.WINDOWLEAVE,
-    pygame.WINDOWENTER,
-    # pygame.MOUSEMOTION,
-    pygame.AUDIODEVICEADDED,
-    pygame.WINDOWSHOWN,
-    pygame.VIDEOEXPOSE,
-    pygame.WINDOWEXPOSED,
-    pygame.WINDOWFOCUSLOST,
-    pygame.WINDOWFOCUSGAINED
-]
+# I'm leaving these here for now as a reference
+# blocked = [
+#     pygame.WINDOWMOVED,
+#     pygame.WINDOWLEAVE,
+#     pygame.WINDOWENTER,
+#     pygame.MOUSEMOTION,
+#     pygame.AUDIODEVICEADDED,
+#     pygame.WINDOWSHOWN,
+#     pygame.VIDEOEXPOSE,
+#     pygame.WINDOWEXPOSED,
+#     pygame.WINDOWFOCUSLOST,
+#     pygame.WINDOWFOCUSGAINED
+# ]
 
 allowed = [
-    # pygame.ACTIVEEVENT,
+    pygame.MOUSEMOTION,
     pygame.MOUSEWHEEL,
+    pygame.MOUSEBUTTONDOWN,
+    pygame.MOUSEBUTTONUP,
     pygame.KEYDOWN,
     pygame.KEYUP,
-    pygame.TEXTINPUT,
-    pygame.MOUSEMOTION,
+    pygame.WINDOWCLOSE
 ]
 
 pygame.event.set_blocked(None) # block all events
@@ -146,10 +149,14 @@ def pasteFromClipboard(should_hold_shift_for_newlines: bool = False):
             usb.keyAction('ENTER', KeyAction.TAP, should_delay=True)
             if should_hold_shift_for_newlines: usb.keyAction('L_SHIFT', KeyAction.RELEASE, should_delay=True)
 
+locked_to_window = False
+mouse_queue_has_input = False
+mouse_queue_last_poll = pygame.time.get_ticks()
+
 while True:
     for event in pygame.event.get():
         # allow us to close the program cleanly
-        if event.type == pygame.QUIT: sys.exit()
+        if event.type in (pygame.QUIT, pygame.WINDOWCLOSE): sys.exit()
 
         # handle key presses
         if event.type in (pygame.KEYDOWN, pygame.KEYUP):
@@ -165,7 +172,7 @@ while True:
                     paste_thread_should_close.set()
                     paste_thread.join()
                 
-                print('later alligator')
+                print('\nlater alligator')
                 sys.exit()
 
             if paste_thread is not None and paste_thread.is_alive():
@@ -194,6 +201,14 @@ while True:
 
                 continue
 
+            # toggle input locking
+            if shortcutIsPressed('lock_to_window'):
+                locked_to_window = False if pygame.event.get_grab() else True
+                pygame.mouse.set_visible(not locked_to_window) # hide the mouse cursor when locked, show it when unlocked
+                pygame.event.set_grab(locked_to_window)
+
+                continue
+
             pygame_key_name = pygame_keycode_map[event.key]
             usb_key_name = pygame_to_usb_map[pygame_key_name]
 
@@ -205,7 +220,58 @@ while True:
             # press or release the destination key
             usb.keyAction(usb_key_name, usb_key_action)
 
-        # TODO mouse control
+        # handle mouse movement
+        if locked_to_window and event.type == pygame.MOUSEMOTION:
+            usb.moveMouseInDirection(event.rel[0], event.rel[1], apply_immediately=False)
+            mouse_queue_has_input = True
+
+        # handle mouse clicks
+        if locked_to_window and event.type in (pygame.MOUSEBUTTONDOWN, pygame.MOUSEBUTTONUP):
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mode = KeyAction.PRESS
+            else:
+                mode = KeyAction.RELEASE
+
+            button = None
+
+            if event.button == 1:
+                button = MouseButton.LEFT
+            elif event.button == 2:
+                button = MouseButton.MIDDLE
+            elif event.button == 3:
+                button = MouseButton.RIGHT
+
+            if button != None:
+                usb.mouseButtonAction(button, mode, apply_immediately=False)
+                mouse_queue_has_input = True
+
+        # handle mouse scrolling
+        # TODO implement scroll smoothing/momentum
+        if locked_to_window and event.type == pygame.MOUSEWHEEL:
+            # the USBtoUSB device can only support vertical scrolling, unfortunately, so we'll ignore any horizontal scrolling pygame tells us about
+            scroll_value = event.y
+
+            if scroll_value != 0:
+                mouse_queue_has_input = True
+                direction = None
+
+                if scroll_value < 0:
+                    direction = MouseScrollDirection.UP
+                else:
+                    direction = MouseScrollDirection.DOWN
+
+                usb.mouseScrollAction(direction, scroll_value, apply_immediately=False)
+
+        # TODO mouse auto-jiggler
+        # if not locked_to_window:
+
+        # send a single updated mouse control packet for movement, clicks, and scrolling
+        if mouse_queue_has_input:
+            mouse_queue_has_input = False
+            usb.sendMouseControlPacket()
+
+
+        # TODO make it so you can pipe text input into the script, to directly make it type out the given text
 
     screen.fill(black)
 
--- usbtousb.py
+++ usbtousb.py
@@ -6,11 +6,21 @@ from time import sleep
 from enum import auto
 from enum import Enum
 from key_mapping import keymap
+from numpy import clip
 
 class KeyAction(Enum):
     TAP = auto()
     RELEASE = auto()
     PRESS = auto()
+
+class MouseButton(Enum):
+    LEFT = auto()
+    MIDDLE = auto()
+    RIGHT = auto()
+
+class MouseScrollDirection(Enum):
+    UP = auto()
+    DOWN = auto()
 
 class UsbToUsb:
     # the settings used to connect here, are those required by the USBtoUSB (Hagstrom Electronics product) hardware
@@ -22,15 +32,34 @@ class UsbToUsb:
     def __init__(self):
         # load in our .env variables
         load_dotenv()
-        port_name = os.environ.get('PORT_NAME');
-        self.delay_lower_bound = os.environ.get('DELAY_LOWER_BOUND');
-        self.delay_upper_bound = os.environ.get('DELAY_UPPER_BOUND');
+        port_name = os.environ.get('PORT_NAME')
+        self.delay_lower_bound = os.environ.get('DELAY_LOWER_BOUND')
+        self.delay_upper_bound = os.environ.get('DELAY_UPPER_BOUND')
+
+        # initialize our mouse data
+        self.mouse_x_position = 0
+        self.mouse_x_position_max = int(os.environ.get('TARGET_SCREEN_X_SIZE')) - 1
+        self.mouse_y_position = 0
+        self.mouse_y_position_max = int(os.environ.get('TARGET_SCREEN_Y_SIZE')) - 1
+        self.left_mouse_pressed = False
+        self.right_mouse_pressed = False
+        self.middle_mouse_pressed = False
+        self.scroll_direction = 0
+        self.scroll_amplitude = 0
+        self.invert_scroll_direction = os.environ.get('INVERT_SCROLL_DIRECTION') == 'True'
 
         # connect to the USBtoUSB com port
         self.com = serial.Serial(port_name, self.baudrate, self.bytesize, self.parity, self.stopbits)
 
-    def sendByte(self, value: int):
-        self.com.write((value).to_bytes(1, 'big', signed = False))
+    def sendBytes(self, value: int, length: int = 1, debug: bool = False):
+        for i in range(0, length):
+            # get just one byte at a time, going from most to least significant
+            which_byte = length - i - 1
+            byte_as_int = value >> (which_byte * 8) & 255
+            byte = (byte_as_int).to_bytes(1, 'big', signed = False)
+
+            if debug: print(byte)
+            self.com.write(byte)
 
     def keyAction(self, key: str, action: KeyAction, should_delay: bool = False):
         key_code_press = keymap[key][0] # code to press down the key
@@ -47,22 +76,104 @@ class UsbToUsb:
                 delay = random.uniform(delay, float(self.delay_upper_bound))
 
         if (action is KeyAction.TAP):
-            self.sendByte(key_code_press)
+            self.sendBytes(key_code_press)
 
             # we need to delay at least a little bit between pressing and releasing the key, otherwise the key may not be released properly
             sleep(0.005)
 
-            self.sendByte(key_code_release) 
+            self.sendBytes(key_code_release) 
         elif (action is KeyAction.PRESS):
-            self.sendByte(key_code_press)
+            self.sendBytes(key_code_press)
         elif (action is KeyAction.RELEASE):
-            self.sendByte(key_code_release)
+            self.sendBytes(key_code_release)
 
         if should_delay: sleep(delay)
 
+    def moveMouseToPosition(self, x_position: int, y_position: int, apply_immediately: bool = True):
+        self.mouse_x_position = x_position
+        self.mouse_y_position = y_position
+
+        if apply_immediately: self.sendMouseControlPacket()
+    
+    def moveMouseInDirection(self, x_change: int, y_change: int, apply_immediately: bool = True):
+        new_x_position = self.mouse_x_position + x_change
+        new_y_position = self.mouse_y_position + y_change
+
+        # make sure our target mouse position stays within the correct range
+        new_x_position = clip([new_x_position], 0, self.mouse_x_position_max)[0]
+        new_y_position = clip([new_y_position], 0, self.mouse_y_position_max)[0]
+        self.mouse_x_position = int(new_x_position)
+        self.mouse_y_position = int(new_y_position)
+
+        if apply_immediately: self.sendMouseControlPacket()
+
+    # TODO decide on a better/consistent naming scheme for KeyAction and its associated variables
+    def mouseButtonAction(self, button: MouseButton, mode: KeyAction, apply_immediately: bool = True):
+        new_button_value = True if mode is KeyAction.PRESS else False
+
+        if button is MouseButton.LEFT:
+            self.left_mouse_pressed = new_button_value
+        elif button is MouseButton.MIDDLE:
+            self.middle_mouse_pressed = new_button_value
+        elif button is MouseButton.RIGHT:
+            self.right_mouse_pressed = new_button_value
+
+        if apply_immediately: self.sendMouseControlPacket()
+
+    def mouseScrollAction(self, direction: MouseScrollDirection, amplitude: int, apply_immediately: bool = True):
+        scroll_direction = 0
+
+        # 0 means up, and 1 means down
+        if direction == MouseScrollDirection.DOWN and not self.invert_scroll_direction:
+            scroll_direction = 1
+        elif direction == MouseScrollDirection.UP and self.invert_scroll_direction:
+            scroll_direction = 1
+        
+        # restrict the amplitude to fit within the (consistently) useable range of the USBtoUSB device
+        new_amplitude = clip([abs(amplitude)], 0, 7)[0]
+        new_amplitude = int(new_amplitude)
+
+        # valid amplitude range for scrolling up, in bytes low to high:       001 thru 111 (1 - 7)
+        # valid amplitude range for scrolling down, in bytes "low" to "high": 111 thru 000 (7 - 0)
+        # We need to handle downward scrolling, because it does a few things differently
+        if scroll_direction == 1:
+            # the USBtoUSB device lets you have a higher amplitude when scrolling down, since 0 counts as the max scroll speed, 8.
+            # I want it to feel balanced, so lower it by 1 to be in line with the allowed amplitude when scrolling up.
+            new_amplitude -= 1
+
+            # the USBtoUSB device expects the amplitude to be inverted when scrolling down, so invert it
+            new_amplitude = 7 - new_amplitude
+
+        self.scroll_direction = scroll_direction
+        self.scroll_amplitude = new_amplitude
+
+        if apply_immediately: self.sendMouseControlPacket()
+    
+    def sendMouseControlPacket(self):
+        self.sendBytes(0) # byte 1 begins the 6 byte mouse control sequence
+        self.sendBytes(self.mouse_x_position, length=2) # byte 2 + byte 3
+        self.sendBytes(self.mouse_y_position, length=2) # byte 4 + byte 5
+
+        # build byte #6: mouse wheel movement
+        control_byte = 0
+
+        control_byte |= self.scroll_direction << 7 # bit 7
+        control_byte |= self.scroll_amplitude << 4 # bits 6, 5, and 4
+        control_byte |= 1 << 3 # bit 3 is always 1, otherwise the entire mouse control packet will be ignored
+        control_byte |= int(self.middle_mouse_pressed) << 2 # bit 2
+        control_byte |= int(self.right_mouse_pressed) << 1 # bit 1
+        control_byte |= int(self.left_mouse_pressed) # bit 0
+
+        # send our newly constructed mouse control packet, which concludes the 6 byte mouse control sequence
+        self.sendBytes(control_byte)
+
+        # always reset the scroll data. Without this, we'd be scrolling forever, or even scrolling when the only action was a mouse movement
+        self.scroll_direction = 0
+        self.scroll_amplitude = 0
+
     # clear the input buffer
     def clear(self):
-        self.sendByte(56)
+        self.sendBytes(56)
 
     def __del__(self):
         # make sure the device buffer is clear
